5531c6d59622bf1776a728a304381714d685244e
gauravraj2601 <gauravraj2601@gmail.com>
2023-07-22 23:00:00 +0530

    day5

--- The Street Bites/src/Pages/Menu.jsx
+++ The Street Bites/src/Pages/Menu.jsx
@@ -3,21 +3,50 @@ import DishCard from '../Components/DishCard'
 import Card from '../Components/card'
 import axios from "axios"
 import { useParams } from 'react-router-dom'
+import "./Menu.css"
 
 const Menu = () => {
   const [data, setData]= useState([])
   const [filterCategory, setFilterCategory]= useState("")
   const {id}= useParams()
-  // if(id==="1"){
-  //   setFilterCategory("")
-  // }
-  // else if (id==="2"){
-  //   setFilterCategory("Pizzas")
-  // }
+ 
+  
+
+  useEffect(() => {
+    if (id === "1") {
+      setFilterCategory(null);
+    } else if (id === "2") {
+      setFilterCategory("Pizzas");
+    }
+    else if (id === "3") {
+      setFilterCategory("Burgers");
+    }
+    else if (id === "4") {
+      setFilterCategory("Pastas");
+    }
+    else if (id === "5") {
+      setFilterCategory("Fried Chicken");
+    }
+    else if (id === "6") {
+      setFilterCategory("Desserts");
+    }
+    else if (id === "7") {
+      setFilterCategory("Drinks");
+    }
+    else if (id === "8") {
+      setFilterCategory(null);
+    }
+    else{
+      setFilterCategory(null)
+    }
+  }, [id]);
+
+
+
   const fetchData=()=>{
     axios
     .get(`https://street-bites-api.onrender.com/menu`,{
-      params: { category:"Pizzas"},
+      params: { category:filterCategory},
     })
     .then((res)=>setData(res.data))
     .catch((error)=>console.log(error))
@@ -32,7 +61,7 @@ const Menu = () => {
   return (
     <div>
         <Card />
-        <div style={{display:"grid", gridTemplateColumns:"repeat(4,1fr)",width:"90%",margin:"auto"}}>
+        <div className='menu_container' style={{display:"grid", gridTemplateColumns:"repeat(4,1fr)",width:"90%",margin:"auto", marginLeft:"7%"}}>
             {data?.map((el)=>(
               <DishCard key={el.id} {...el} />
 
--- The Street Bites/src/Routes/Navbar.jsx
+++ The Street Bites/src/Routes/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   
 
     return (
-        <div className="navbar" style={{ display: 'flex', backgroundColor: "#121618", justifyContent: 'space-between' }}>
+        <div className="navbar" style={{ display: 'flex', backgroundColor: "#121618", justifyContent: 'space-between'}}>
           <div style={{ marginLeft: "10%",display:"flex" }}>
             <NavLink to={"/"}><img src={logo} alt="Logo" /></NavLink>
             <img style={{width:"100px", height:"77px",margin:"10px 0 0 -13px"}} src="http://www.thestreetbites.com/images/india-india-flag.gif" alt="imgError" />
